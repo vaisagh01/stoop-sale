175282ca67c1a7d6e2364ae55dd1b5d6a345e72f
vaisagh01 <vaisuro45@gmail.com>
2024-07-12 20:43:49 +0530

    updated

--- src/pages/Page3.jsx
+++ src/pages/Page3.jsx
@@ -17,11 +17,19 @@ const Page3 = () => {
             <h1 className='text-[#ff7591] text-4xl font-bold'>Get in touch with us!</h1>
             <input type="text" className='w-1/2 border-4 shadow-solid border-neutral-600 rounded-xl p-10 bg-white' />
         </div>
-        <section className='self-center cursor-pointer w-screen flex gap-10 justify-center'>                
-            <motion.img whileHover={{y:-20}} src="/facebook.png" className='h-24' alt="" />
-            <motion.img whileHover={{y:-20}}  src="/twitter.png" className='h-24' alt="" />
-            <motion.img whileHover={{y:-20}}  src="/instagram.png" className='h-24' alt="" />
-            <motion.img whileHover={{y:-20}}  src="/linkedin.png" className='h-24' alt="" />
+        <section className='self-center cursor-pointer w-screen flex gap-10 justify-center'> 
+            <a href="http://facebook.com">
+                <motion.img whileHover={{y:-20}}  src="/facebook.png" className='h-24' alt="" />
+            </a>  
+            <a href="http://twitter.com">
+                <motion.img whileHover={{y:-20}}  src="/twitter.png" className='h-24' alt="" />
+            </a>
+            <a href="http://instagram.com">
+                <motion.img whileHover={{y:-20}}  src="/instagram.png" className='h-24' alt="" />
+            </a>
+            <a href="http://linkedin.com">
+                <motion.img whileHover={{y:-20}}  src="/linkedin.png" className='h-24' alt="" />
+            </a>             
         </section>
     </div>
   )
